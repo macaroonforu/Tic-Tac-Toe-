a62f120bfdca15ddbba2e30ba9ddc0702d2ec085
macaroonforu <121368271+macaroonforu@users.noreply.github.com>
2023-02-21 23:06:23 -0500

    Update GameState.h

--- GameState.h
+++ GameState.h
@@ -1,14 +1,7 @@
 //
 //  GameState.h
 //  TicTacToe
-//
-//  Created by Tarek Abdelrahman on 2019-06-07.
-//  Modified by Tarek Abdelrahman on 2020-09-17.
-//  Copyright © 2019-2020 Tarek Abdelrahman. All rights reserved.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
+
 //
 //  ECE244 Student: Do NOT modify this file
 

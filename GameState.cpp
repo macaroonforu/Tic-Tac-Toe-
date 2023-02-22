7593a7f94283e26682babbbd88dbab8af7354947
macaroonforu <121368271+macaroonforu@users.noreply.github.com>
2023-02-21 23:05:54 -0500

    Update GameState.cpp

--- GameState.cpp
+++ GameState.cpp
@@ -1,15 +1,6 @@
 //
 //  GameState.cpp
 //  TicTacToe  
-//
-//  Created by Tarek Abdelrahman on 2019-06-07.
-//  Modified by Tarek Abdelrahman on 2020-09-17.
-//  Copyright © 2019-2020 Tarek Abdelrahman. All rights reserved.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-//
 
 #include "globals.h"
 #include "GameState.h"

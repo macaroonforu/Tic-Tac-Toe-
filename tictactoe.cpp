544e1ef2913d12c770f88942278765038e3bc5b8
macaroonforu <121368271+macaroonforu@users.noreply.github.com>
2023-02-21 23:06:54 -0500

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,16 +1,6 @@
 //
 //  main.cpp
 //  TicTacTo
-//
-//  Created by Tarek Abdelrahman on 2019-06-07.
-//  Modified by Tarek Abdelrahman on 2020-09-17.
-//  Copyright © 2019-2020 Tarek Abdelrahman. All rights reserved.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-//
-//  ECE244 Student: Complete the skeletal code of the main function in this file
 
 
 #include <iostream>
